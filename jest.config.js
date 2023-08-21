d14e845b3732907bf6f3d81117836edc1441e850
patrick <pa.trickjlp@gmail.com>
2023-08-21 11:35:24 +0200

    fix build

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': [
-      'ts-jest',
-      {
-        babelConfig: {
-          plugins: ['babel-plugin-transform-import-meta'],
-        },
-      },
-    ],
-  },
+  testEnvironment: 'node'
 };
