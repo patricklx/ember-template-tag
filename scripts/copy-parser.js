b5b4e0d6c411e16f1d2c2b855a0f4e72da39c585
Jake Bixby <jbixby@auditboard.com>
2023-09-05 07:55:13 -0700

    chore: add ParserPlugin to copied babel/parser export

--- scripts/copy-parser.js
+++ scripts/copy-parser.js
@@ -5,6 +5,6 @@ const parserPath = require.resolve('@babel/parser');
 let parserContent = fs.readFileSync(parserPath).toString().split('\n').slice(0, -5).join('\n');
 parserContent = '// @ts-nocheck\n' + parserContent;
 parserContent += '\n';
-parserContent += 'export { Parser, mixinPlugins }\n';
+parserContent += 'export { Parser, ParserPlugin, mixinPlugins }\n';
 
 fs.writeFileSync(path.join(__dirname, '..', 'lib/babel-parser.ts'), parserContent);
